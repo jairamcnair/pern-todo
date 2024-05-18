3777e2a5017eb6b32a793229a2cf81d2cdbb79f1
Jaira McNair <jaimcnair10@gmail.com>
2024-05-18 16:17:35 -0400

    worked on InputTodo

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, {Fragment} from "react";
 import './App.css';
 
+// components
+import InputTodo from "./components/InputTodo";
+import ListTodos from "./components/ListTodos";
+import EditTodo from "./components/EditTodo";
+
 function App() {
   return (
     <Fragment>
-      
+      <InputTodo/>
     </Fragment>
   );
 }
